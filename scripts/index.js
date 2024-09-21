3861e5ef16b5a6671a851ba751ae5c911864e812
Christopher Verrell <c.verrell07@gmail.com>
2024-09-21 15:48:28 +0700

    fix: image sources

--- scripts/index.js
+++ scripts/index.js
@@ -2,30 +2,30 @@ const projectData = {
     grab: {
         title: "Grab AI Destination Explorer",
         links: [
-            { text: "App Repository", url: "https://github.com/cverrell07/Destination-Explorer-SolaGratia", logo: "/assets/github-logo.png"},
-            { text: "AI Module", url: "https://github.com/cverrell07/destination-explorer-ai-module", logo: "/assets/github-logo.png"},
-            { text: "Ptch Deck", url: "https://drive.google.com/file/d/1YcdJF06x8uXC-BJdQGabAflKS23PSvJ7/view?usp=sharing", logo: "/assets/pdf-logo.png"}
+            { text: "App Repository", url: "https://github.com/cverrell07/Destination-Explorer-SolaGratia", logo: "./assets/github-logo.png"},
+            { text: "AI Module", url: "https://github.com/cverrell07/destination-explorer-ai-module", logo: "./assets/github-logo.png"},
+            { text: "Ptch Deck", url: "https://drive.google.com/file/d/1YcdJF06x8uXC-BJdQGabAflKS23PSvJ7/view?usp=sharing", logo: "./assets/pdf-logo.png"}
         ]
     },
     libeery: {
         title: "Libeery",
         links: [
-            { text: "Notion", url: "https://slow-headlight-e6d.notion.site/Libeery-82db6e44b97b46e0b6ef326f6c283b65?pvs=4", logo: "/assets/notion-logo.png"},
-            { text: "App Repository", url: "https://github.com/cverrell07/Libeery-App", logo: "/assets/github-logo.png"},
-            { text: "UI/UX Design", url: "https://www.figma.com/design/RjVv3tG32HVFfgXFLUPjFg/Libeery?node-id=81-265&t=A9RspXmpbXjSlAkM-1", logo: "/assets/figma-logo.png"},
+            { text: "Notion", url: "https://slow-headlight-e6d.notion.site/Libeery-82db6e44b97b46e0b6ef326f6c283b65?pvs=4", logo: "./assets/notion-logo.png"},
+            { text: "App Repository", url: "https://github.com/cverrell07/Libeery-App", logo: "./assets/github-logo.png"},
+            { text: "UI/UX Design", url: "https://www.figma.com/design/RjVv3tG32HVFfgXFLUPjFg/Libeery?node-id=81-265&t=A9RspXmpbXjSlAkM-1", logo: "./assets/figma-logo.png"},
         ]
     },
     "lapak-kita": {
         title: "lapak kita",
         links: [
-            { text: "GitHub", url: "https://github.com/cverrell07/HCI-Lapak-Kita", logo: "/assets/github-logo.png"},
-            { text: "UI/UX Design", url: "https://www.figma.com/design/xoEt1RbEbRCb26M36SdTZu/lapak-kita?node-id=313-1658&t=aG05SUG0xmdtoMld-1", logo: "/assets/figma-logo.png"}
+            { text: "GitHub", url: "https://github.com/cverrell07/HCI-Lapak-Kita", logo: "./assets/github-logo.png"},
+            { text: "UI/UX Design", url: "https://www.figma.com/design/xoEt1RbEbRCb26M36SdTZu/lapak-kita?node-id=313-1658&t=aG05SUG0xmdtoMld-1", logo: "./assets/figma-logo.png"}
         ]
     },
     cookability: {
         title: "cookability",
         links: [
-            { text: "Prototype", url: "https://www.figma.com/design/x4c4ZOagoWKRHaJxyBdhEX/cookability?node-id=0-1&t=Heod0MzrUbNwjK6r-1", logo: "/assets/figma-logo.png"},
+            { text: "Prototype", url: "https://www.figma.com/design/x4c4ZOagoWKRHaJxyBdhEX/cookability?node-id=0-1&t=Heod0MzrUbNwjK6r-1", logo: "./assets/figma-logo.png"},
         ]
     }
 };
